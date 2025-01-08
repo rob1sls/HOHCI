cb0954261109c06cd6cefae862c2a6ab64325a34
rob1sls <76230012+rob1sls@users.noreply.github.com>
2025-01-08 11:38:51 +0100

    final vm

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://130.83.163.25:8080/api/v1',  // API URL avec l'IP publique
-  defaultProfilePhotoUrl: 'http://130.83.163.25/assets/images/6.jpg',  // URL de l'image de profil par défaut
-  defaultCoverPhotoUrl: 'http://130.83.163.25/assets/images/cover.jpg'  // URL de l'image de couverture par défaut
+  apiUrl: 'http://202.61.205.20:8080/api/v1',  // API URL avec l'IP publique
+  defaultProfilePhotoUrl: 'http://202.61.205.20/assets/images/6.jpg',  // URL de l'image de profil par défaut
+  defaultCoverPhotoUrl: 'http://202.61.205.20/assets/images/cover.jpg'  // URL de l'image de couverture par défaut
 };
